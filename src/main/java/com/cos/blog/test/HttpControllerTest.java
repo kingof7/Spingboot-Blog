93357cfcb35e1e0f1ddad90302e97164c9198cfe
songha <thdgk6304@naver.com>
2021-09-05 22:47:31 +0900

    16강

--- src/main/java/com/cos/blog/test/HttpControllerTest.java
+++ src/main/java/com/cos/blog/test/HttpControllerTest.java
@@ -12,6 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HttpControllerTest {
 	
+	@GetMapping("/http/lombok")
+	public String lombokTest() {
+		//Member m = new Member(1, "ssar", "1234", "email");
+		Member m = Member.builder().username("ssar").password("1234").email("vlfl0@naver.com").build(); // 1.생성자 순서안지켜도됨 2.set할때 순서 기억안해도됨
+		System.out.println(TAG+"getter : " + m.getUsername());
+		m.setUsername("cos");
+		System.out.println(TAG+"setter : " + m.getUsername());		
+		return "lombok test 완료";
+	}
+	
+	private static final String TAG = "HttpController Test : ";
+	
 	// http://localhost:8081/http/get (select)
 	@GetMapping("/http/get")
 	public String getTest(Member m) { // @RequestParam int id, @RequestParam String id은 개별적으로 받을때
@@ -24,16 +36,21 @@ public class HttpControllerTest {
 //		return "post 요청 : "+text;
 //	}
 	
+//	@PostMapping("/http/post") // application/json
+//	public String postTest(@RequestBody Member m) {
+//		return "post 요청 : " + m.getId() +", " + m.getUsername() +", " + m.getPassword() + ", " + m.getEmail();
+//	}
+	
 	// http://localhost:8081/http/post (insert)
 		@PostMapping("/http/post") // application/json
-		public String postTest(@RequestBody String text) {
-			return "post 요청 : "+text;
+		public String postTest(@RequestBody Member m) { //MessageConverter (springboot) -- json으로 요청했을 시
+			return "post 요청 : " + m.getId() +", " + m.getUsername() +", " + m.getPassword() + ", " + m.getEmail();
 		}
 	
 	// http://localhost:8081/http/put (update)
 	@PutMapping("/http/put")
-	public String putTest() {
-		return "put 요청";
+	public String putTest(@RequestBody Member m) {
+		return "put 요청 : " + m.getId()+", "+m.getUsername()+", "+m.getPassword()+", "+m.getEmail();
 	}
 	
 	// http://localhost:8081/http/delete (delete)
--- src/main/java/com/cos/blog/test/Member.java
+++ src/main/java/com/cos/blog/test/Member.java
@@ -1,54 +1,32 @@
 package com.cos.blog.test;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+
+//@Getter
+//@Setter
+//@AllArgsConstructor // 생성자
+//@RequiredArgsConstructor // final 붙여야함
+
+@Data //getter + setter
+@NoArgsConstructor // 빈생성자
 public class Member {
 	private int id;
 	private String username;
 	private String password;
 	private String email;
 	
-public Member() {
-	// TODO Auto-generated constructor stub
-}
-
-public Member(int id, String username, String password, String email) {
-	super();
-	this.id = id;
-	this.username = username;
-	this.password = password;
-	this.email = email;
-}
-
-public int getId() {
-	return id;
-}
-
-public void setId(int id) {
-	this.id = id;
-}
-
-public String getUsername() {
-	return username;
-}
-
-public void setUsername(String username) {
-	this.username = username;
-}
-
-public String getPassword() {
-	return password;
-}
-
-public void setPassword(String password) {
-	this.password = password;
-}
-
-public String getEmail() {
-	return email;
-}
-
-public void setEmail(String email) {
-	this.email = email;
-}
-
+	@Builder // 파라미터 갯수 자동조절
+	public Member(int id, String username, String password, String email) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.email = email;
+	}
+		
 
 }
